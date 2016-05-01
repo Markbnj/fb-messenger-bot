79c478e381c46f08bdc8a7beeba39bd5f61ecab7
Mark Betz <betz.mark@gmail.com>
2016-05-01 16:32:36 -0400

    Changed error handling to raise an exception rather than return a string; added comment.

--- aws/lambda/handlers/verification_handler.py
+++ aws/lambda/handlers/verification_handler.py
@@ -5,14 +5,26 @@ logger = logging.getLogger()
 
 
 def verify(event, context, tokens):
+    """
+    Handles the API verification step from Facebook, which consists of a GET
+    to the webhook with the following querystring args:
+
+        hub.verify_token = the verification token you supplied when setting up
+        the web hook.
+
+        hub.challenge = the challenge value Facebook expects back.
+
+    The expected value for the verify_token is stored in config/tokens.json,
+    which is read in webhook.pay and passed in the tokens param to this call.
+    """
     my_verify_token = tokens.get("verifyToken")
     verify_token = event['query'].get("hub.verify_token")
     challenge = event['query'].get("hub.challenge")
     if verify_token == my_verify_token:
         if not challenge:
-            return "400 Bad Request; missing challenge"
+            raise Exception("400 Bad Request; missing challenge")
         else:
             return challenge
     else:
-        return "403 Forbidden; verification token mismatch"
+        raise Exception("403 Forbidden; verification token mismatch")
 
