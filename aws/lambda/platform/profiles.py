85201fc23df7175ff1f31f5cebd915a58d55f481
Mark Betz <betz.mark@gmail.com>
2016-05-07 01:16:52 -0400

    Added a platform package and modules for functions to call back out to facebook.

---
+++ aws/lambda/platform/profiles.py
@@ -0,0 +1,29 @@
+from config import settings
+import json
+import logging
+import os
+import requests
+
+
+logger = logging.getLogger()
+
+
+default_fields = "first_name,last_name,locale,timezone,gender"
+
+
+def get(user_id, fields=default_fields):
+    """
+    Calls the graph API to return the user profile for a specific
+    page-scoped user ID.
+    Params:
+        user_id: the page-scoped user id.
+        fields: comma-delimited list of fields to return
+    """
+    url = settings.get("graphProfileUrl").format(user_id, fields, settings.get("pageToken"))
+    logger.debug("Calling {}".format(url))
+    response = requests.get(url)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        logger.debug("Graph API request error; status: {}, response: {}".format(response.status_code, response.text))
+        return None
---
+++ aws/lambda/platform/sender.py
@@ -0,0 +1,10 @@
+from config import settings
+import json
+
+
+def send():
+    """
+    Sends a structured or unstructured message to a specific page-scoped
+    user ID.
+    """
+    pass
