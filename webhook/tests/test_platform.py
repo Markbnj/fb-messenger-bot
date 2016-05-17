d4f8501a890b8edf82300ce1ee3f3a074352c47a
Mark Betz <betz.mark@gmail.com>
2016-05-17 16:16:33 -0400

    Added test for generic message with buttons.

--- webhook/tests/test_platform.py
+++ webhook/tests/test_platform.py
@@ -122,3 +122,62 @@ class TestMakeButtonMessage(unittest.TestCase):
         buttons.append(messages.make_postback_button("This is a test title", "This is a test payload"))
         message = messages.make_message("1789953497899630", "button_message", {"prompt_text": "Here lies a button"}, buttons)
         self.assertEqual(message, self.expected)
+
+
+class TestMakeGenericMessage(unittest.TestCase):
+    def setUp(self):
+        logger.info("\n\n>>>>TEST CASE: {}".format(self.id()))
+
+        self.expected = json.loads("""
+        {
+            "recipient": {
+                "id": "1789953497899630"
+            },
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "This is a test title",
+                                "image_url": "http://some.where/but_not_here.png",
+                                "item_url": "http://some.where/but_not_here",
+                                "subtitle": "This is a test subtitle",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "http://some.where/but_not_here",
+                                        "title": "This is a test title"
+                                    },
+                                    {
+                                        "type": "postback",
+                                        "title": "This is a test title",
+                                        "payload": "This is a test payload"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+        """)
+
+    def test(self):
+        """
+        Tests messages.make_message by checking that the returned dict matches
+        self.expected, defined above.
+        """
+        message = messages.make_message("1789953497899630", "generic_message")
+        buttons = []
+        buttons.append(messages.make_url_button("This is a test title", "http://some.where/but_not_here"))
+        buttons.append(messages.make_postback_button("This is a test title", "This is a test payload"))
+        messages.add_message_element(
+            message,
+            "This is a test title",
+            "This is a test subtitle",
+            "http://some.where/but_not_here.png",
+            "http://some.where/but_not_here",
+            buttons)
+        self.assertEqual(message, self.expected)
