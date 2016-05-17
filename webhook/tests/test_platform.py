4519f9fbd2a3f812c551254eb4bc9a8c72f98576
Mark Betz <betz.mark@gmail.com>
2016-05-17 14:41:30 -0400

    Added a test case for make_message() with an image message.

--- webhook/tests/test_platform.py
+++ webhook/tests/test_platform.py
@@ -25,7 +25,7 @@ sh.setLevel(logging.DEBUG)
 logger.addHandler(sh)
 
 
-class TestMakeMessage(unittest.TestCase):
+class TestMakeTextMessage(unittest.TestCase):
     def setUp(self):
         logger.info("\n\n>>>>TEST CASE: {}".format(self.id()))
 
@@ -46,4 +46,33 @@ class TestMakeMessage(unittest.TestCase):
         self.expected, defined above.
         """
         message = messages.make_message("1789953497899630", "text_message", {"message_text": "This is a basic test message."})
+        self.assertEqual(message, self.expected)
+
+
+class TestMakeImageMessage(unittest.TestCase):
+    def setUp(self):
+        logger.info("\n\n>>>>TEST CASE: {}".format(self.id()))
+
+        self.expected = json.loads("""
+        {
+            "recipient": {
+                "id": "1789953497899630"
+            },
+            "message": {
+                "attachment": {
+                    "type": "image",
+                    "payload": {
+                        "url": "http://some.where/but_not_here.png"
+                    }
+                }
+            }
+        }
+        """)
+
+    def test(self):
+        """
+        Tests messages.make_message by checking that the returned dict matches
+        self.expected, defined above.
+        """
+        message = messages.make_message("1789953497899630", "image_message", {"image_url": "http://some.where/but_not_here.png"})
         self.assertEqual(message, self.expected)
